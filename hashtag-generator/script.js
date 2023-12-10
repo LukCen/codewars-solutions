87ae81fede8d3c965b1e4f8970cfa223bc857ed8
Lukasz Cena <lbjcena@gmail.com>
2023-12-10 19:40:59 +0100

    first iteration

---
+++ hashtag-generator/script.js
@@ -0,0 +1,28 @@
+'use strict'
+
+function generateHashtag (str) {
+  if (str !== '' && str.trim().length > 0 && str.length <= 140) {
+    const arrayfied = str.split(' ')
+    const result = []
+    arrayfied.forEach((e) => {
+      const first = e.slice(0, 1).toUpperCase()
+      const completed = first + e.slice(1)
+      result.push(completed)
+    })
+    if (result.join('').length >= 140) {
+      return false
+    }
+    return `#${result.join('')}`
+  }
+  return false
+}
+
+const string = '#'
+
+const test = '     '
+
+// console.log(test.length)
+// console.log(test.trim().length)
+
+console.log(generateHashtag('Codewars'))
+console.log(generateHashtag('code' + ' '.repeat(140) + 'wars'))
